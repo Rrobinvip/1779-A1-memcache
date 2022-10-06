58646f3b3d5747628267c50be18d46332ef4e522
Haoyu <rrobinvip@live.cn>
2022-10-06 19:03:55 -0400

    All page build completed. Base frontend structure completed

--- frontend/form.py
+++ frontend/form.py
@@ -1,9 +1,14 @@
-from re import L
 from flask_uploads import UploadSet, IMAGES, configure_uploads
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, FileField
 from flask_wtf.file import FileField, FileRequired, FileAllowed, DataRequired
+# from wtforms import StringField, SubmitField, FileField
+from wtforms.fields import IntegerField, SelectField, StringField, SubmitField, FileField
+from wtforms.validators import DataRequired, NumberRange
 from frontend import app
+
+'''
+This module gives all forms needed for every pages. Forms are written in classes to acquire decent ability on expension.
+'''
 
 pictures = UploadSet('photos', IMAGES)
 configure_uploads(app, pictures)
@@ -20,7 +25,8 @@ class UploadForm(FlaskForm):
         "Key",
         validators=[ 
             DataRequired()
-        ]
+        ],
+        render_kw={"placeholder": "Key"}
     )
     submit = SubmitField("Upload")
 
@@ -29,6 +35,25 @@ class SearchForm(FlaskForm):
         "Key",
         validators=[
             DataRequired()
-        ]
+        ],
+        render_kw={"placeholder": "Key"}
     )
     submit = SubmitField("Search")
+
+class ConfigForm(FlaskForm):
+    size = IntegerField(
+        "size",
+        validators=[
+            DataRequired(),
+            NumberRange(min=1, max=1024)
+        ],
+        render_kw={"placeholder": "Size ~MB"}
+    )
+    replacement_policy = SelectField(
+        "replacement_policy",
+        choices=[(1, "Drop least use"), (2, "Random drop")]
+    )
+    submit = SubmitField("Apply")
+
+class ClearForm(FlaskForm):
+    clear = SubmitField("Clear Memcache")
--- frontend/main.py
+++ frontend/main.py
@@ -1,9 +1,12 @@
-from crypt import methods
-from fileinput import filename
-from stat import filemode
-from telnetlib import SE
+# from crypt import methods
+# from fileinput import filename
+# from stat import filemode
+# from telnetlib import SE
+# from traceback import clear_frames
+
+from glob import escape
 from flask import render_template, url_for, request, redirect
-from flask import json, flash
+from flask import json, flash, jsonify
 from frontend import app
 
 # Picture upload form
@@ -14,6 +17,10 @@ from frontend.data import Data
 
 # Search form
 from frontend.form import SearchForm
+
+# Config form 
+from frontend.form import ConfigForm
+from frontend.form import ClearForm
 
 sql_connection = Data()
 
@@ -34,13 +41,7 @@ def upload_picture():
         sql_connection.add_entry(key, filename)
         return redirect(url_for("upload_picture"))
     
-    return render_template("upload.html", form = picture_form)
-
-@app.route("/allpairs")
-def all_pairs():
-    data = sql_connection.inspect_all_entries()
-    
-    return render_template("all_pairs.html", items=data, tag2_selected=True)
+    return render_template("upload.html", form=picture_form)
 
 @app.route("/search", methods=["GET", "POST"])
 def search_key():
@@ -49,7 +50,21 @@ def search_key():
     upload_time = None
     key = None
 
-    if request.method == "POST" and search_form.validate_on_submit():
+    # TODO 
+    if request.method == "GET" and "key" in request.args:
+        key = escape(request.args.get("key"))
+        data = sql_connection.search_key(key)
+
+        if len(data) == 0:
+            print("No data")
+            flash("No image with this key.")
+        else:
+            filename = data[0][2]
+            upload_time = data[0][3]
+            print("Filename: {} upload_time: {}".format(filename, upload_time))
+
+
+    elif request.method == "POST" and search_form.validate_on_submit():
         key = search_form.key.data
         data = sql_connection.search_key(key)
 
@@ -67,3 +82,44 @@ def search_key():
                            filename=filename, 
                            upload_time=upload_time,
                            key=key)
+
+@app.route("/allpairs")
+def all_pairs():
+    data = sql_connection.inspect_all_entries()
+    
+    return render_template("all_pairs.html", items=data, tag2_selected=True)
+
+
+@app.route("/config", methods=["GET", "POST"])
+def memcache_config():
+    config_form = ConfigForm()
+    clear_form = ClearForm()
+
+    if request.method == "GET" and "size" in request.args and "policy" in request.args:
+        # TODO
+        # make API call to backend to config memcache
+        flash("Update success")
+        print("swws")
+
+    if request.method == "POST" and config_form.validate_on_submit():
+        size = config_form.size.data
+        choice = config_form.replacement_policy.data
+
+        # TODO 
+        # make API call to backend to config memcache
+        flash("Update success")
+        print("++++++++ size and choice: ",size, choice)
+
+    if request.method == "POST" and clear_form.validate_on_submit():
+        # TODO make API call to backend to clear memcache
+        flash("Memcache cleared.")
+        
+    return render_template("config.html", form1=config_form, form2=clear_form, tag3_selected=True)
+
+@app.route("/status")
+def memcache_status():
+    # TODO 
+    # make API call to backend to accquire data of memcache status
+    test_list = [[0,1,2,3,4]]
+
+    return render_template("status.html", items=test_list, tag4_selected=True)
