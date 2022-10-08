9fea5b3f59d7f703fb3d3010f8da8e2a7cc7d81a
Alan Wang <alanwang@Alans-MacBook-Pro.local>
2022-10-07 21:26:31 -0400

    Add more function in helper backend

--- backend/helper.py
+++ backend/helper.py
@@ -1,4 +1,6 @@
 import sys
+import random
+from datetime import datetime
 
 class helper:
     def __init__(self):
@@ -20,4 +22,33 @@ class helper:
         else:
             result = True
             return result
+    
+    #This function helps reandom remove an item from memcache
+    #it will return the removed value, if the value is none, then the remove
+    #action fails
+    def randomRemove(self,memcache):
+        key = random.choice(memcache.keys())
+        removed_value = memcache.pop(key,None)
+        return removed_value
+    
+    #This function helps remove the least recent use key
+    #It will return the removed value, the the value is none, then the remove
+    #action fails
+    def removeLeastRecentUse(self,memcache,usage):
+        minValue = min(usage.values())
+        key = usage.get(minValue)
+        removed_value = None
+        if key is not None:
+            memcache.pop(key,None)
+        return removed_value
+    
+    #This function helps remove key and value from the memcache
+    def removeKey(self,memcache,key):
+        removed_value = memcache.pop(key,None)
+        return removed_value
+
+
+
+
+    
     
