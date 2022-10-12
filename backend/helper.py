fb95b2481445699fec94467c3ca4a5d060261339
Alan Wang <alanwang@Alans-MacBook-Pro.local>
2022-10-12 19:32:42 -0400

    fix helper size function

--- backend/helper.py
+++ backend/helper.py
@@ -1,19 +1,22 @@
 import sys
-import random
-from datetime import datetime
-from flask import jsonify
 
-
-    
 def byteSize(memcache):
     '''
     This function return the size of the memcache.
     '''
     size = sys.getsizeof(memcache)
+    keys = memcache.keys()
+    values = memcache.values()
+    #get size of all keys
+    for key in keys:
+        size = size + sys.getsizeof(key)
+    #get size of all values
+    for value in values:
+        size = size + sys.getsizeof(value)
     return size
 
 def mbytesize(memcache):
-    return byteSize(memcache)/1024
+    return byteSize(memcache)/1000000
 
 def checkSize(memcache,config_size):
     '''
@@ -21,40 +24,12 @@ def checkSize(memcache,config_size):
     else return false.
     '''
     result = False
-    size = sys.getsizeof(memcache)
+    size = mbytesize(memcache)
     if size > config_size:
         return result
     else:
         result = True
         return result
-
-def randomRemove(memcache):
-    '''
-    This function helps reandom remove an item from memcache. It will return the removed value, if the value is none, then the remove 
-    action fails.
-    '''
-    key = random.choice(memcache.keys())
-    removed_value = memcache.pop(key,None)
-    return removed_value
-
-def removeLeastRecentUse(memcache,usage):
-    '''
-    This function helps remove the least recent use key. It will return the removed value, the the value is none, then the remove
-    action fails.
-    '''
-    minValue = min(usage.values())
-    key = usage.get(minValue)
-    removed_value = None
-    if key is not None:
-        memcache.pop(key,None)
-    return removed_value
-
-def removeKey(memcache,key):
-    '''  
-    This function helps remove key and value from the memcache
-    '''
-    removed_value = memcache.pop(key,None)
-    return removed_value
 
 
 
